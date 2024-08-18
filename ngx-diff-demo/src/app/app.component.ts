96ec679a5a542d291810de80bf0d33e0a7ea06d3
max <maxcheung@optusnet.com.au>
2024-08-18 16:52:14 +1000

    add ngx-diff-demo

--- ngx-diff-demo/src/app/app.component.ts
+++ ngx-diff-demo/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBySideDiffComponent } from 'ngx-diff';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBySideDiffComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngx-diff-demo';
+  title = 'ngx-diff';
+  
+  myObject1 = {
+    name: 'John',
+    age: 30,
+    isActive: true,
+    hobbies: ['reading', 'gaming', 'coding']
+  };
+  myObject2 = {
+    name: 'John',
+    age: 29,
+    isActive: false,
+    hobbies: ['reading', 'coding', 'writing']
+  };  
+  oldDocumentContents =  JSON.stringify(this.myObject1, null, 2);
+  newDocumentContents =  JSON.stringify(this.myObject2, null, 2);
 }
